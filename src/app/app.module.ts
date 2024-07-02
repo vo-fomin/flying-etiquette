8276f9ee8d58e66006f4590add4b92cba7e2a7be
Volodymyr Fomin <w260698@gmail.com>
2024-07-02 17:34:47 +0300

    Flexmonster added and configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexmonsterPivotModule } from 'ngx-flexmonster';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FlexmonsterPivotModule,
     BrowserModule,
     AppRoutingModule
   ],
